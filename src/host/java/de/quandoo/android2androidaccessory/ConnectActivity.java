537a7bb65fed254d5dad8d45bf4f8f28f1e1e6e7
ligi <ligi@ligi.de>
2014-06-24 16:43:45 +0200

    nice up code

--- src/host/java/de/quandoo/android2androidaccessory/ConnectActivity.java
+++ src/host/java/de/quandoo/android2androidaccessory/ConnectActivity.java
@@ -52,7 +52,7 @@ public class ConnectActivity extends ActionBarActivity {
         finish();
     }
 
-    private boolean searchForUsbAccessory(HashMap<String, UsbDevice> deviceList) {
+    private boolean searchForUsbAccessory(final HashMap<String, UsbDevice> deviceList) {
         for (UsbDevice device:deviceList.values()) {
             if (isUsbAccessory(device)) {
 
@@ -68,11 +68,11 @@ public class ConnectActivity extends ActionBarActivity {
         return false;
     }
 
-    private boolean isUsbAccessory(UsbDevice device) {
+    private boolean isUsbAccessory(final UsbDevice device) {
         return (device.getProductId() == 0x2d00) || (device.getProductId() == 0x2d01);
     }
 
-    private boolean initAccessory(UsbDevice device) {
+    private boolean initAccessory(final UsbDevice device) {
 
         final UsbDeviceConnection connection = mUsbManager.openDevice(device);
 
@@ -80,24 +80,23 @@ public class ConnectActivity extends ActionBarActivity {
             return false;
         }
 
-        stringControlTransfer(connection, 0, "quandoo");
-        stringControlTransfer(connection, 1, "Android2AndroidAccessory");
-        stringControlTransfer(connection, 2, "showcasing android2android USB communication");
-        stringControlTransfer(connection, 3, "0.1");
-        stringControlTransfer(connection, 4, "http://quandoo.de");
-        stringControlTransfer(connection, 5, "42");
+        initStringControlTransfer(connection, 0, "quandoo"); // MANUFACTURER
+        initStringControlTransfer(connection, 1, "Android2AndroidAccessory"); // MODEL
+        initStringControlTransfer(connection, 2, "showcasing android2android USB communication"); // DESCRIPTION
+        initStringControlTransfer(connection, 3, "0.1"); // VERSION
+        initStringControlTransfer(connection, 4, "http://quandoo.de"); // URI
+        initStringControlTransfer(connection, 5, "42"); // SERIAL
 
-        connection.controlTransfer(0x40, 53, 0, 0, new byte[]{}, 0, 100);
+        connection.cont .controlTransfer(0x40, 53, 0, 0, new byte[]{}, 0, 100);
 
         connection.close();
 
         return true;
     }
 
-    private void stringControlTransfer(final UsbDeviceConnection deviceConnection,
-                                       final int index,
-                                       final String string) {
+    private void initStringControlTransfer(final UsbDeviceConnection deviceConnection,
+                                           final int index,
+                                           final String string) {
         deviceConnection.controlTransfer(0x40, 52, 0, index, string.getBytes(), string.length(), 100);
-
     }
 }
